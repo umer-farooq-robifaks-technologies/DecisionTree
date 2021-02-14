2982af78504ff31311096b0903d7883783fdd091
Umer Farooq <34151564+umer-farooq-robifaks-technologies@users.noreply.github.com>
2021-02-14 13:22:08 +0500

    Add files via upload

--- DecisionTreeMain.py
+++ DecisionTreeMain.py
@@ -1,4 +1,4 @@
-from Project1DecisionTree import DecisionTree, DisplayTree, Testing
+from DecisionTree import DecisionTree, DisplayTree, Testing
 import pandas as pd
 from sklearn.model_selection import train_test_split
 global root
