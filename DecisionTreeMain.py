1e97dfe20c3c1eb92d6bc6df3228ee30a65b117d
Umer Farooq <34151564+umer-farooq-robifaks-technologies@users.noreply.github.com>
2021-02-14 13:17:43 +0500

    Add files via upload

---
+++ DecisionTreeMain.py
@@ -0,0 +1,24 @@
+from Project1DecisionTree import DecisionTree, DisplayTree, Testing
+import pandas as pd
+from sklearn.model_selection import train_test_split
+global root
+
+IrreleventClasses=[]
+Dataset = input("Enter Name of Dataset File: ") 
+TargetClass = input("Enter Name of Target Class: ")
+n = int(input("Enter number of Irrelevent Classes: ")) 
+for i in range(0, n): 
+    element = input('Enter Irrelevent Class: ')   
+    IrreleventClasses.append(element) 
+      
+
+fulldata = pd.read_csv(Dataset)
+TrainingData,Test=train_test_split(fulldata, test_size=0.2, random_state=42)
+
+   
+root=DecisionTree(TrainingData, TargetClass, IrreleventClasses)
+print(root.label)
+
+DisplayTree(root)
+
+Testing(Test,root)
